3305b35f2544da58ff1987f198fb3db5d784c3de
Daniel Calvin <arockne2003@yahoo.com>
2021-07-23 09:18:09 -0700

    commit tree 461c2f063bfa1392ddadbaf004d2c8ea869a2421

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution here
+//implement findMathcing
+  //passes in an array of drivers names
+  //passes in a string
+  //returns an array of the matching list of drivers
+function findMatching(drivers, string) {
+  return drivers.filter(driver => driver.toLowerCase() === string.toLowerCase());
+}
+
+//implement fuzzyMatch
+  //passes in an array of drivers names
+  //passes in a string to match
+  //returns all drivers whose name begin with the provided letters
+function fuzzyMatch(drivers, string) {
+  return drivers.filter(driver => driver.slice(0, string.length).toLowerCase() === string.toLowerCase());
+}
+
+//implement matchName
+  //takes in an array of driver objects
+  //passes in a string
+  //each driver object has two properties
+    //name
+    //hometown
+  //return each element whose name matches the string argument
+function matchName(drivers, string) {
+  return drivers.filter(driver => driver.name.toLowerCase() === string.toLowerCase());
+}
